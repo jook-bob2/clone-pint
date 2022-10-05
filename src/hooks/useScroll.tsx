ef10604051429e449dd30759b97ebf25ddfa7f9a
jook-bob2 <qwefk123@naver.com>
2022-10-06 01:15:38 +0900

    ..

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -9,39 +9,45 @@ export default function useScroll() {
         x: 0,
         y: 0,
     })
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState<boolean>(false)
     const [direction, setDirection] = useState<DirectionType>(undefined)
 
-    const listener = useCallback((value: ScrollState, mounted: boolean) => {
-        if (mounted) {
-            const newScrollY = window.scrollY
-            const newScrollX = window.scrollX
-            if (value.y > newScrollY) {
-                setDirection('up')
-            } else if (value.y < newScrollY) {
-                setDirection('down')
-            } else if (value.x > newScrollX) {
-                setDirection('left')
-            } else if (value.x < newScrollX) {
-                setDirection('right')
+    const listener = useCallback(
+        (e: Event, value: ScrollState, mounted: boolean) => {
+            if (mounted) {
+                const newScrollY = window.scrollY
+                const newScrollX = window.scrollX
+                if (value.y > newScrollY) {
+                    setDirection('up')
+                } else if (value.y < newScrollY) {
+                    setDirection('down')
+                } else if (value.x > newScrollX) {
+                    setDirection('left')
+                } else if (value.x < newScrollX) {
+                    setDirection('right')
+                }
+                setState({ x: newScrollX, y: newScrollY })
+                setLoading(false)
+            } else {
+                e.preventDefault()
+                e.stopPropagation()
             }
-            setState({ x: newScrollX, y: newScrollY })
-            setLoading(false)
-        }
-    }, [])
+        },
+        [state],
+    )
 
     useEffect(() => {
         let mounted = true
         if (window) {
             window.addEventListener(
                 'scroll',
-                debounce(() => listener(state, mounted), delay),
+                debounce((e: Event) => listener(e, state, mounted), delay),
             )
         }
 
         return () => {
             mounted = false
-            window.removeEventListener('scroll', () => listener(state, mounted))
+            window.removeEventListener('scroll', (e: Event) => listener(e, state, mounted))
         }
     }, [state])
 
